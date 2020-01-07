18d3683d8e6c783eaa6595aa5372af19cfc95885
AnhThu <Ngothianhthu161299@gmail.com>
2020-01-07 21:18:02 +0700

    Lesson01-code-07.01.2020

---
+++ lesson02-variables-datatype/src/main/java/j4t/lesson02_variables_datatype/App.java
@@ -0,0 +1,13 @@
+package j4t.lesson02_variables_datatype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
